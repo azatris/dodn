5c836b125201c3a1f93987f088e6e6fbca69d571
Silver Taza <azatris@gmail.com>
2014-10-15 02:46:07 +0100

    Refactor, add TODO

--- network.py
+++ network.py
@@ -1,7 +1,7 @@
 __author__ = 'Azatris'
 
 import layer
-
+import numpy as np
 
 class Network(object):
     def __init__(self, architecture, initial_weight_magnitude):
@@ -10,29 +10,46 @@ class Network(object):
             for neurons, inputs_per_neuron
             in zip(architecture[1:], architecture[:-2])
         ]
-        self.layers.append(layer.Softmax(architecture[-1], architecture[-2], initial_weight_magnitude))
-        
-        for l in xrange(0, len(self.layers)):
-            print "Created weight matrtix in layer %d with shape %s"%(l, self.layers[l].weights.shape)
-            print "Created biases vector in layer %d with shape %s"%(l, self.layers[l].biases.shape)
+
+        self.layers.append(
+            layer.Softmax(
+                architecture[-1], architecture[-2], initial_weight_magnitude
+            )
+        )
+
+        ### INFO ###
+        for L in xrange(0, len(self.layers)):
+            print "Created weight matrix in layer {} with shape {}".format(
+                L, self.layers[L].weights.shape
+            )
+            print "Created biases vector in layer {} with shape {}".format(
+                L, self.layers[L].biases.shape
+            )
 
     def feed_forward(self, x, return_all=False):
-        act=[None]*(len(self.layers)+1) #inputs + layer activations
-        act[0]=x
-        for l in xrange(0,len(self.layers)):
-            act[l+1] = self.layers[l].feed_forward(act[l])
-        if return_all is True:
-            return act
-        return act[-1]
-    
-    def feed_backward(self, error, a):
-        assert len(a)==(len(self.layers)+1) #a should have feats at 0th index
-        deltas = [None]*len(self.layers)
-        # some notation identieties, z=Wx+b, h=f(z), hence dh/dz depends on non-linearity and z is just linear transform
-        # to update params you need dh/dz (deltas) and prev layer activations h(l-1)
-        # but you need also pass the signal through linear part, referred as eh below
-        #deltas[-1] = error #note, here we make a shortcut, that the error w.r.t cross-entropy and 1ofK targets is actually a correct gradient
-        eh = error #this one is supposed to keep dh/dz*W.T, above deltas are only dh/dz
-        for l in xrange(len(self.layers)-1,-1,-1):
-            deltas[l], eh = self.layers[l].feed_backward(eh, a[l+1])
+        activations = np.empty(len(self.layers) + 1, dtype=object)
+        activations[0] = x
+        for L in xrange(0, len(self.layers)):
+            activations[L+1] = self.layers[L].feed_forward(activations[L])
+        if return_all:
+            return activations
+        return activations[-1]
+
+    # TODO: What is going on here?
+    def feed_backward(self, error, activations):
+        assert len(activations) == (len(self.layers) + 1)
+        deltas = np.empty(len(self.layers), dtype=object)
+
+        # some notation identities, z=Wx+b, h=f(z), hence dh/dz depends on
+        # non-linearity and z is just linear transform
+        # to update params you need dh/dz (deltas) and prev layer activations
+        # h(l-1) but you need also pass the signal through linear part,
+        # referred as eh below
+
+        # deltas[-1] = error #note, here we make a shortcut, that the error
+        # w.r.t cross-entropy and 1ofK targets is actually a correct gradient
+        eh = error  # this one is supposed to keep dh/dz*W.T,
+                    # above deltas are only dh/dz
+        for L in xrange(len(self.layers) - 1, -1, -1):
+            deltas[L], eh = self.layers[L].feed_backward(eh, activations[L+1])
         return deltas
